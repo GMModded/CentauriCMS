4a6130878fa3ad94c5467201108f5e11e941d333
GMModded <gmmodded@googlemail.com>
2019-12-11 08:00:47 +0100

    [UPDATE] Start progress of validator.js for EditorComponent.js 1/2

---
+++ Assets/js/Utility/EditorUtility/Validator.js
@@ -0,0 +1,10 @@
+Centauri.Utility.EditorUtility.Validator = function(form) {
+    // const inputs = [];
+
+    // $form = $(form, $editor);
+
+    // $("input", $form).on("focus", function() {
+    //     var id = $(this).attr("id");
+        
+    // });
+};
