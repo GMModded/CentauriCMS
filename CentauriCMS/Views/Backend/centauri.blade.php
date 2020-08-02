7de576971cf8b3ce1d427272ab9ac7faebed15a3
Mati Sediqi <m.sediqi@schommer-media.de>
2020-08-02 23:03:06 +0200

    fix

--- CentauriCMS/Views/Backend/centauri.blade.php
+++ CentauriCMS/Views/Backend/centauri.blade.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>CentauriCMS » {{ $title ?? $title }}</title>
+        <title>CentauriCMS » {{ isset($title) ? $title : "" }}</title>
         <link rel="stylesheet" href="{{ \Centauri\CMS\Helper\GulpRevHelper::include(
             \Centauri\CMS\Utility\PathUtility::getAbsURL("public/backend"),
             "css",
